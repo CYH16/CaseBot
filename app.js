bb883ca083624990a54267c8e55fd5aebebd5e59
CYH16 <h5s12s20s@yahoo.com.tw>
2017-09-30 20:49:03 +0800

    1. prototype

---
+++ app.js
@@ -0,0 +1,122 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: "381ddbed-8120-4d12-b565-d39e21004bd2",
+    appPassword: "cfVhYKuirOgUT7ovt9L0tYo"
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Get data
+const Data = JSON.parse(require('fs').readFileSync('./case_data.json', 'utf8'));
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, [
+	function (session) {
+		session.send("嗨你好");
+		session.beginDialog('scene');
+	},
+]);
+
+bot.dialog('scene', [
+    function (session) {
+        session.send("%s", Data["scene"]);
+		session.beginDialog('PE');
+    }
+]);
+
+bot.dialog('PE', [
+    function (session) {
+		if (Array.isArray(Data["PE"])) {
+			var reply = new builder.Message().setText(session, Data["PE"][0]).addAttachment({contentType:'audio', contentUrl:Data["PE"][1]});
+			session.send(reply);
+			session.beginDialog('tools');
+		} else {
+			session.send("%s", Data["PE"]);
+			session.beginDialog('tools');
+		}    
+	}
+]);
+
+bot.dialog('tools', [
+    function (session) {
+		var tools = Object.keys(Data["tools"]);
+		tools.push("我要診斷");
+		var card = new builder.ThumbnailCard(session).title('接下來你想做什麼呢？').text('如果想要診斷的話就直接打"我要診斷"吧！').buttons(tools.map(choice => new builder.CardAction.imBack(session, choice, choice)));
+        var message = new builder.Message(session).addAttachment(card);
+		builder.Prompts.text(session, message);
+        //builder.Prompts.choice(session,"接下來你想做什麼呢？\n",Object.keys(Data["tools"]).join('|').concat("|下診斷"),{listStyle: builder.ListStyle["button"],retryPrompt:"請選擇我們現在有的工具喔，有".concat((Object.keys(Data["tools"])).join('、')).concat("；除此之外，你也可以下診斷。")});
+    },
+	function (session, results){
+		if (results.response == "我要診斷" || results.response == "下診斷" || results.response == "診斷" || results.response == "我要下診斷") {
+			session.replaceDialog("diagnosis");
+		} else if (results.response in Data["tools"]) {
+			if (Array.isArray(Data["tools"][results.response])) {
+				var reply = new builder.Message().setText(session, Data["tools"][results.response][0]).addAttachment({contentType:'image/jpeg', contentUrl:Data["tools"][results.response][1]});
+				session.send(reply);
+				session.replaceDialog("tools")
+			} else {
+				session.send("%s", Data["tools"][results.response]);
+				session.replaceDialog("tools");
+			}
+		} else {
+			session.send("請選擇我們現在有的工具喔~");
+			session.replaceDialog("tools");
+		}
+	}
+]);
+
+bot.dialog('diagnosis', [
+    function (session) {
+		var help = ['等等我還想做其他檢查', '太難了吧~給我點選項~'];
+		var card = new builder.ThumbnailCard(session).title('你的診斷是？').text('直接打出你的診斷吧！').buttons(help.map(choice => new builder.CardAction.imBack(session, choice, choice)));
+        var message = new builder.Message(session).addAttachment(card);
+		builder.Prompts.text(session, message);
+    },
+	function (session, results){
+		if (results.response == "等等我還想做其他檢查") {
+			session.replaceDialog("tools");
+		} else if (results.response == "太難了吧~給我點選項~") {
+			session.replaceDialog("options");
+		} else if (Data["diagnosis"].indexOf(results.response) >= 0) {
+			session.send("答對了！這位病人罹患的是急性心包膜炎(acute pericarditis)。")
+			session.replaceDialog("summary");
+		} else {
+			session.send("錯囉！這位病人罹患的是急性心包膜炎(acute pericarditis)。")
+			session.replaceDialog("summary");
+		}
+	}
+]);
+
+bot.dialog('options', [
+    function (session) {
+		var options = Data["options"];
+		var card = new builder.ThumbnailCard(session).title('好吧').text('可能的診斷有以下幾個：').buttons(options.map(choice => new builder.CardAction.imBack(session, choice, choice)));
+        var message = new builder.Message(session).addAttachment(card);
+		builder.Prompts.text(session, message);
+    },
+	function (session, results){
+		if (Data["diagnosis"].indexOf(results.response) >= 0) {
+			session.send("答對了！這位病人罹患的是急性心包膜炎(acute pericarditis)。")
+			session.replaceDialog("summary");
+		} else {
+			session.send("錯囉！這位病人罹患的是急性心包膜炎(acute pericarditis)。")
+			session.replaceDialog("summary");
+		}
+	}
+]);
+
+bot.dialog('summary', [
+    function (session) {
+        session.send("%s", Data["summary"]);
+    }
+]);
